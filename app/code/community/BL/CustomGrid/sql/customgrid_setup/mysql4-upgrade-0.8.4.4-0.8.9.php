36e5bf23d7c6bda89760ddc37aa31260933531fd
Benoît <benoit.leulliette@gmail.com>
2012-07-04 20:23:55 +0200

    lots of fixes / changes to 0.8.9 setup file

--- app/code/community/BL/CustomGrid/sql/customgrid_setup/mysql4-upgrade-0.8.4.4-0.8.9.php
+++ app/code/community/BL/CustomGrid/sql/customgrid_setup/mysql4-upgrade-0.8.4.4-0.8.9.php
@@ -17,6 +17,14 @@ $installer  = $this;
 $installer->startSetup();
 $connection = $installer->getConnection();
 
+$tables = array(
+    'grid'         => $installer->getTable('customgrid/grid'),
+    'grid_column'  => $installer->getTable('customgrid/grid_column'),
+    'grid_profile' => $installer->getTable('customgrid/grid_profile'),
+    'grid_role'    => $installer->getTable('customgrid/grid_role'),
+    'grid_user'    => $installer->getTable('customgrid/grid_user'),
+);
+
 /**
 * New tables
 */
@@ -24,7 +32,7 @@ $connection = $installer->getConnection();
 // Note: the "grid_profile" one and related columns from "grid_role" / "grid_user" are for potential further use
 
 $installer->run("
-CREATE TABLE IF NOT EXISTS `{$this->getTable('customgrid/grid_profile')}` (
+CREATE TABLE IF NOT EXISTS `{$tables['grid_profile']}` (
 `profile_id` int(10) unsigned NOT NULL auto_increment,
 `grid_id` int(10) unsigned NOT NULL,
 `name` varchar(255) character set utf8 NOT NULL,
@@ -35,10 +43,10 @@ CREATE TABLE IF NOT EXISTS `{$this->getTable('customgrid/grid_profile')}` (
 `default_filters` text character set utf8 default NULL,
 PRIMARY KEY (`profile_id`),
 KEY `FK_CUSTOM_GRID_GRID_PROFILE_GRID` (`grid_id`),
-CONSTRAINT `FK_CUSTOM_GRID_GRID_PROFILE_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$this->getTable('customgrid/grid')}` (`grid_id`) ON DELETE CASCADE,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_PROFILE_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$tables['grid']}` (`grid_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `{$this->getTable('customgrid/grid_role')}` (
+CREATE TABLE IF NOT EXISTS `{$tables['grid_role']}` (
 `grid_role_id` int(10) unsigned NOT NULL auto_increment,
 `grid_id` int(10) unsigned NOT NULL,
 `role_id` int(10) unsigned NOT NULL,
@@ -49,23 +57,23 @@ PRIMARY KEY (`grid_role_id`),
 KEY `FK_CUSTOM_GRID_GRID_ROLE_GRID` (`grid_id`),
 KEY `FK_CUSTOM_GRID_GRID_ROLE_ROLE` (`role_id`),
 KEY `FK_CUSTOM_GRID_GRID_ROLE_DEFAULT_PROFILE` (`default_profile_id`),
-CONSTRAINT `FK_CUSTOM_GRID_GRID_ROLE_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$this->getTable('customgrid/grid')}` (`grid_id`) ON DELETE CASCADE,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_ROLE_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$tables['grid']}` (`grid_id`) ON DELETE CASCADE,
 CONSTRAINT `FK_CUSTOM_GRID_GRID_ROLE_ROLE` FOREIGN KEY (`role_id`) REFERENCES `{$this->getTable('admin/role')}` (`role_id`) ON DELETE CASCADE,
-CONSTRAINT `FK_CUSTOM_GRID_GRID_ROLE_DEFAULT_PROFILE` FOREIGN KEY (`default_profile_id`) REFERENCES `{$this->getTable('customgrid/grid_profile')}` (`profile_id`) ON DELETE SET NULL,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_ROLE_DEFAULT_PROFILE` FOREIGN KEY (`default_profile_id`) REFERENCES `{$tables['grid_profile']}` (`profile_id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `{$this->getTable('customgrid/grid_user')}` (
+CREATE TABLE IF NOT EXISTS `{$tables['grid_user']}` (
 `grid_user_id` int(10) unsigned NOT NULL auto_increment,
 `grid_id` int(10) unsigned NOT NULL,
 `user_id` mediumint(9) unsigned NOT NULL,
 `default_profile_id` int(10) unsigned default NULL,
 PRIMARY KEY (`grid_user_id`),
 KEY `FK_CUSTOM_GRID_GRID_USER_GRID` (`grid_id`),
-KEY `FK_CUSTOM_GRID_GRID_USER_ROLE` (`user_id`),
+KEY `FK_CUSTOM_GRID_GRID_USER_USER` (`user_id`),
 KEY `FK_CUSTOM_GRID_GRID_USER_DEFAULT_PROFILE` (`default_profile_id`),
-CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$this->getTable('customgrid/grid')}` (`grid_id`) ON DELETE CASCADE,
-CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_ROLE` FOREIGN KEY (`user_id`) REFERENCES `{$this->getTable('admin/user')}` (`user_id`) ON DELETE CASCADE,
-CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_DEFAULT_PROFILE` FOREIGN KEY (`default_profile_id`) REFERENCES `{$this->getTable('customgrid/grid_profile')}` (`profile_id`) ON DELETE SET NULL,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_GRID` FOREIGN KEY (`grid_id`) REFERENCES `{$tables['grid']}` (`grid_id`) ON DELETE CASCADE,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_USER` FOREIGN KEY (`user_id`) REFERENCES `{$this->getTable('admin/user')}` (`user_id`) ON DELETE CASCADE,
+CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_DEFAULT_PROFILE` FOREIGN KEY (`default_profile_id`) REFERENCES `{$tables['grid_profile']}` (`profile_id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
 
@@ -74,37 +82,37 @@ CONSTRAINT `FK_CUSTOM_GRID_GRID_USER_DEFAULT_PROFILE` FOREIGN KEY (`default_prof
 */
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'max_custom_column_id',
     'int(10) unsigned NOT NULL default 0 AFTER `max_attribute_column_id`'
 );
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'default_page_behaviour',
     "enum ('default','force_original','force_custom') character set utf8 default NULL"
 );
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'default_limit_behaviour',
     "enum ('default','force_original','force_custom') character set utf8 default NULL"
 );
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'default_sort_behaviour',
     "enum ('default','force_original','force_custom') character set utf8 default NULL"
 );
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'default_dir_behaviour',
     "enum ('default','force_original','force_custom') character set utf8 default NULL"
 );
 
 $connection->addColumn(
-    $installer->getTable('customgrid/grid'),
+    $tables['grid'],
     'default_filter_behaviour',
     "enum ('default','force_original','force_custom','merge_default','merge_on_original','merge_on_custom') character set utf8 default NULL"
 );
@@ -113,47 +121,62 @@ $connection->addColumn(
 * Columns changes for "customgrid_grid" table
 */
 
-$connection->changeColumn(
-    $installer->getTable('customgrid/grid'),
-    'default_direction',
-    'default_dir',
-    "enum('asc', 'desc') character set utf8 default NULL"
-);
+if ($connection->tableColumnExists($tables['grid'], 'default_direction')) {
+    $connection->changeColumn(
+        $tables['grid'],
+        'default_direction',
+        'default_dir',
+        "enum('asc','desc') character set utf8 default NULL"
+    );
+}
 
-$connection->changeColumn(
-    $installer->getTable('customgrid/grid'),
-    'default_filters',
-    'default_filter',
-    'text character set utf8 default NULL'
-);
+if ($connection->tableColumnExists($tables['grid'], 'default_filters')) {
+    $connection->changeColumn(
+        $tables['grid'],
+        'default_filters',
+        'default_filter',
+        'text character set utf8 default NULL'
+    );
+}
 
 /**
 * New column for "customgrid_column" table
 */
 
 // This one is for potential further use
+
 $connection->addColumn(
-    $installer->getTable('customgrid/grid_column'),
+    $tables['grid_column'],
     'profile_id',
     'int(10) unsigned default NULL'
 );
 
 $connection->addConstraint(
-    'FK_CUSTOM_GRID_GRID_COLUMN_PROFILE_GRID_PROFILE'
+    'FK_CUSTOM_GRID_GRID_COLUMN_PROFILE_GRID_PROFILE',
+    $tables['grid_column'],
+    'profile_id',
+    $tables['grid_profile'],
+    'profile_id',
+    'set null'
 );
 
-// Column changes for "customgrid_column" table
-
-$installer->getConnection()->changeColumn(
-    $installer->getTable('customgrid/grid_column'),
-    'origin',
-    "enum('grid','collection','attribute','custom') character set utf8 NOT NULL default 'grid'"
-);
-
-$installer->getConnection()->addColumn(
-    $installer->getTable('customgrid/grid_column'),
+$connection->addColumn(
+    $tables['grid_column'],
     'custom_params',
     'text character set utf8 default NULL'
 );
+
+/**
+* Column changes for "customgrid_column" table
+*/
+
+if ($connection->tableColumnExists($tables['grid_column'], 'origin')) {
+    $connection->changeColumn(
+        $installer->getTable('customgrid/grid_column'),
+        'origin',
+        'origin',
+        "enum('grid','collection','attribute','custom') character set utf8 NOT NULL default 'grid'"
+    );
+}
 
 $installer->endSetup();
