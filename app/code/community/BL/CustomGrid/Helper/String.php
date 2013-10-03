9651b98dfe66e68c8d75a3edab6e5f7bc20f34cd
mage-eag <benoit.leulliette@gmail.com>
2013-10-03 18:13:37 +0200

    fix for rendering configuration values containing HTML entities

--- app/code/community/BL/CustomGrid/Helper/String.php
+++ app/code/community/BL/CustomGrid/Helper/String.php
@@ -13,6 +13,31 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Core
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class BL_CustomGrid_Helper_String extends Mage_Core_Helper_Abstract
 {
     const ICONV_CHARSET = 'UTF-8'; 
@@ -216,5 +241,29 @@ class BL_CustomGrid_Helper_String extends Mage_Core_Helper_Abstract
             }
         }
         return $array;
+    }
+    
+    public function htmlDoubleEscape($data, $allowedTags=null)
+    {
+        if (is_array($data)) {
+            $result = array();
+            foreach ($data as $item) {
+                $result[] = $this->htmlDoubleEscape($item);
+            }
+        } else {
+            if (strlen($data)) {
+                if (is_array($allowedTags) and !empty($allowedTags)) {
+                    $allowed = implode('|', $allowedTags);
+                    $result = preg_replace('/<([\/\s\r\n]*)(' . $allowed . ')([\/\s\r\n]*)>/si', '##$1$2$3##', $data);
+                    $result = htmlspecialchars($result, ENT_COMPAT, 'UTF-8', true);
+                    $result = preg_replace('/##([\/\s\r\n]*)(' . $allowed . ')([\/\s\r\n]*)##/si', '<$1$2$3>', $result);
+                } else {
+                    $result = htmlspecialchars($data, ENT_COMPAT, 'UTF-8', true);
+                }
+            } else {
+                $result = $data;
+            }
+        }
+        return $result;
     }
 }
